913ff90d1976107435ed1351a00fdf02e92aaadb
ak1103dev <ak.apichan1103@gmail.com>
2017-02-24 15:06:02 +0700

    can return string

--- SRC/DES/main.c
+++ SRC/DES/main.c
@@ -1,7 +1,22 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
-void main(int argc, char **argv) {
+unsigned char* des(unsigned char* buffer, int start, int end) {
+  unsigned char text[8];
+  int i;
+  for (i = start; i < end; i++) {
+    text[i % 8] = buffer[i];
+    printf("%x ", text[i % 8]);
+  }
+  printf("\n");
+  return text;
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    printf("error\n");
+    return 0;
+  }
   FILE *ptr;
   int filesize;
   char* filename = argv[1];
@@ -16,11 +31,14 @@ void main(int argc, char **argv) {
 
   fread(buffer, sizeof(buffer), 1, ptr);
 
+  unsigned char* text = malloc(8);
+  text = des(buffer, 0, 8);
+
   int i;
-  for (i = 0; i < filesize; i++) {
-    printf("%x ", buffer[i]);
+  for (i = 0; i < 8; i++) {
+    printf("%x ", text[i]);
   }
-  printf("\n");
-  // printf("file size = %d\n", filesize);
+
+  return 1;
 }
 

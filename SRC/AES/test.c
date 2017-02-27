3e66939c85198f1a95eadc745b591d5be821b678
ak1103dev <ak.apichan1103@gmail.com>
2017-02-27 14:04:44 +0700

    get arg

--- SRC/AES/test.c
+++ SRC/AES/test.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdint.h>
@@ -8,26 +9,107 @@
 
 #include "aes.h"
 
+static void strToHex(unsigned char* str, uint8_t* hex);
 static void phex(uint8_t* str);
 static void test_encrypt_ecb_verbose(void);
 
-int main(void)
-{
-    test_encrypt_ecb_verbose();
+int main(int argc, char **argv) {
+  if (argc < 4) {
+    printf("error\n");
     return 0;
+  }
+  unsigned char* keyHex = argv[2];
+  uint8_t key[16];
+  strToHex(keyHex, key);
+
+  printf("key = ");
+  phex(key);
+
+  FILE *ptr;
+  int filesize;
+  char* filename = argv[1];
+
+  ptr = fopen(filename, "rb");
+
+  fseek(ptr, 0, SEEK_END);
+  filesize = ftell(ptr);
+  fseek(ptr, 0, SEEK_SET);
+
+  FILE *write_ptr;
+  write_ptr = fopen(argv[3],"wb");
+
+  int num = filesize / 64;
+  int reminder = filesize % 64;
+  if (reminder != 0) num += 1;
+  int i, j;
+  for (i = 0; i < num; i++) {
+    uint8_t buffer[64];
+
+    fread(buffer, sizeof(buffer), 1, ptr);
+
+    /*
+    printf("plaintext = ");
+    int j;
+    for (j = 0; j < 64; j++) {
+      printf("%.2x ", buffer[j]);
+    }
+    printf("\n");
+    */
+
+    if (i == num - 1 && reminder != 0) {
+      for (j = reminder; j < 64; j++) {
+        buffer[j] = 0;
+      }
+    }
+
+    fwrite(buffer, sizeof(buffer), 1, write_ptr);
+    //free(buffer);
+  }
+  fclose(write_ptr);
+
+  return 1;
+}
+
+unsigned char hexToDec(char x) {
+  switch (x) {
+      case 'a':
+      case 'A':
+        return 10;
+      case 'b':
+      case 'B':
+        return 11;
+      case 'c':
+      case 'C':
+        return 12;
+      case 'd':
+      case 'D':
+        return 13;
+      case 'e':
+      case 'E':
+        return 14;
+      case 'f':
+      case 'F':
+        return 15;
+      default:
+        return x - '0';
+    }
+}
+static void strToHex(unsigned char* str, uint8_t* hex) {
+  int i;
+  for (i = 0; i < 16; i++) {
+    hex[i] = 16 * hexToDec(str[2*i]) + hexToDec(str[2*i + 1]);
+  }
 }
 
 // prints string as hex
-static void phex(uint8_t* str)
-{
-    unsigned char i;
-    for(i = 0; i < 16; ++i)
-        printf("%.2x", str[i]);
-    printf("\n");
+static void phex(uint8_t* str) {
+  unsigned char i;
+  for(i = 0; i < 16; ++i)
+    printf("%.2x ", str[i]);
+  printf("\n");
 }
 
-static void test_encrypt_ecb_verbose(void)
-{
+static void test_encrypt_ecb_verbose(void) {
     // Example of more verbose verification
 
     // uint8_t i, buf[64], buf2[64];

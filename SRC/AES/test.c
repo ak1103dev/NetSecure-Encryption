58ec2a53c5feeaaa0818382a143a6e5e55954cff
ak1103dev <ak.apichan1103@gmail.com>
2017-02-27 15:11:05 +0700

    aes ok

--- SRC/AES/test.c
+++ SRC/AES/test.c
@@ -11,7 +11,6 @@
 
 static void strToHex(unsigned char* str, uint8_t* hex);
 static void phex(uint8_t* str);
-static void test_encrypt_ecb_verbose(void);
 
 int main(int argc, char **argv) {
   if (argc < 4) {
@@ -44,17 +43,11 @@ int main(int argc, char **argv) {
   int i, j;
   for (i = 0; i < num; i++) {
     uint8_t buffer[64];
+    uint8_t cipher[64];
+
+    memset(cipher, 0, 64);
 
     fread(buffer, sizeof(buffer), 1, ptr);
-
-    /*
-    printf("plaintext = ");
-    int j;
-    for (j = 0; j < 64; j++) {
-      printf("%.2x ", buffer[j]);
-    }
-    printf("\n");
-    */
 
     if (i == num - 1 && reminder != 0) {
       for (j = reminder; j < 64; j++) {
@@ -62,8 +55,11 @@ int main(int argc, char **argv) {
       }
     }
 
-    fwrite(buffer, sizeof(buffer), 1, write_ptr);
-    //free(buffer);
+    for (j = 0; j < 4; j++) {
+      AES128_ECB_encrypt(buffer + (j*16), key, cipher + (j*16));
+    }
+
+    fwrite(cipher, sizeof(cipher), 1, write_ptr);
   }
   fclose(write_ptr);
 
@@ -113,7 +109,6 @@ static void test_encrypt_ecb_verbose(void) {
     // Example of more verbose verification
 
     // uint8_t i, buf[64], buf2[64];
-    uint8_t i, buf[64];
 
     // 128bit key
     uint8_t key[16] =        { (uint8_t) 0x2b, (uint8_t) 0x7e, (uint8_t) 0x15, (uint8_t) 0x16, (uint8_t) 0x28, (uint8_t) 0xae, (uint8_t) 0xd2, (uint8_t) 0xa6, (uint8_t) 0xab, (uint8_t) 0xf7, (uint8_t) 0x15, (uint8_t) 0x88, (uint8_t) 0x09, (uint8_t) 0xcf, (uint8_t) 0x4f, (uint8_t) 0x3c };
@@ -123,29 +118,5 @@ static void test_encrypt_ecb_verbose(void) {
                                (uint8_t) 0x30, (uint8_t) 0xc8, (uint8_t) 0x1c, (uint8_t) 0x46, (uint8_t) 0xa3, (uint8_t) 0x5c, (uint8_t) 0xe4, (uint8_t) 0x11, (uint8_t) 0xe5, (uint8_t) 0xfb, (uint8_t) 0xc1, (uint8_t) 0x19, (uint8_t) 0x1a, (uint8_t) 0x0a, (uint8_t) 0x52, (uint8_t) 0xef,
                                (uint8_t) 0xf6, (uint8_t) 0x9f, (uint8_t) 0x24, (uint8_t) 0x45, (uint8_t) 0xdf, (uint8_t) 0x4f, (uint8_t) 0x9b, (uint8_t) 0x17, (uint8_t) 0xad, (uint8_t) 0x2b, (uint8_t) 0x41, (uint8_t) 0x7b, (uint8_t) 0xe6, (uint8_t) 0x6c, (uint8_t) 0x37, (uint8_t) 0x10 };
 
-    memset(buf, 0, 64);
-    // memset(buf2, 0, 64);
-
-    // print text to encrypt, key and IV
-    printf("ECB encrypt verbose:\n\n");
-    printf("plain text:\n");
-    for(i = (uint8_t) 0; i < (uint8_t) 4; ++i)
-    {
-        phex(plain_text + i * (uint8_t) 16);
-    }
-    printf("\n");
-
-    printf("key:\n");
-    phex(key);
-    printf("\n");
-
-    // print the resulting cipher as 4 x 16 byte strings
-    printf("ciphertext:\n");
-    for(i = 0; i < 4; ++i)
-    {
-        AES128_ECB_encrypt(plain_text + (i*16), key, buf+(i*16));
-        phex(buf + (i*16));
-    }
-    printf("\n");
 }
 
